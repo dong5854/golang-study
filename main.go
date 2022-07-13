fbab71024cd0066dc5e0c46445d09be62e1e535b
dongyoung <dong5854@gmail.com>
2022-07-13 11:37:41 +0900

    Slow URL Checker

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/dong5854/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("requests failed")
+
 func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "Greeting")
-	err := dictionary.Update(baseWord, "Second")
-	if err != nil {
-		fmt.Println(err)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	dictionary.Delete(baseWord)
-	word, err2 := dictionary.Search(baseWord)
-	if err2 != nil {
-		fmt.Println(err2)
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
 	}
-	fmt.Println(word)
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
