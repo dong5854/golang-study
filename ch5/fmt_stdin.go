5d6579f242c4a9d1d7f0ae6ca599af6670c83272
dong young <dong5854@gmail.com>
2022-07-17 02:28:02 +0900

    ch5 textIO

---
+++ ch5/fmt_stdin.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+
+	var a int
+	var b int
+
+	n, err := fmt.Scanln(&a, &b)
+	if err != nil {
+		fmt.Println(err)
+		stdin.ReadString('\n') // 표준 입력 스트립 지우기
+	} else {
+		fmt.Println(n, a, b)
+	}
+	n, err = fmt.Scanln(&a, &b)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(n, a, b)
+	}
+}
